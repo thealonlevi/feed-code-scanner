325127de15df8ff89a12d5de4b63689da82d7239
Alon Levi <levialon@proton.me>
2025-01-21 10:51:15 +0200

    Update

--- server/main.py
+++ server/main.py
@@ -29,5 +29,5 @@ def webhook():
         return "Event received", 200
 
 if __name__ == '__main__':
-    # Run the Flask app
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Run the Flask app on port 80
+    app.run(host='0.0.0.0', port=80, debug=True)
